19fc6fbe6ecbcbea17ddff46694ec2fb166343ac
Gabriel Faria <gfmsoares@hotmail.com>
2023-12-06 13:57:49 -0300

    Changed trees
    - Now use the interaction system
    - Chop animation

---
+++ Assets/Scripts/Controller/TreeController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeController : MonoBehaviour, IInteractable
+{
+    public GameObject itemPrefab;
+    public int hp = 5;
+    public int lootID = 1;
+
+    private HitPoints hitPoints;
+
+    void Start()
+    {
+        hitPoints = new HitPoints(hp);
+    }
+
+    public void Interact()
+    {
+        Debug.Log("Chop");
+        hitPoints.ApplyDamage(1);
+        GetComponent<Animator>().SetTrigger("Chop");
+        
+        if (hitPoints.IsDead)
+        {
+            GetComponent<TreeView>().ChopTree();
+            itemPrefab.GetComponent<ItemPickupView>().itemId = lootID;
+            Instantiate(itemPrefab, transform);
+        }
+    }
+}
+
--- Assets/Scripts/View/TreeView.cs
+++ Assets/Scripts/View/TreeView.cs
@@ -6,31 +6,11 @@ public class TreeView : MonoBehaviour
 {
     public GameObject aliveTree;
     public GameObject deadTree;
-    public GameObject itemPrefab;
 
-    private HitPoints hitPoints;
-    public int hp = 5;
-    public int lootID = 1;
-
-    void Start()
+    public void ChopTree()
     {
-        hitPoints = new HitPoints(hp);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Axe"))
-        {
-            Debug.Log("Chop");
-            hitPoints.ApplyDamage(1);
-            if (hitPoints.IsDead)
-            {
-                deadTree.SetActive(true);
-                aliveTree.SetActive(false);
-                GetComponent<Collider2D>().enabled = false;
-                itemPrefab.GetComponent<ItemPickupView>().itemId = lootID;
-                Instantiate(itemPrefab, transform);
-            }
-        }
+        deadTree.SetActive(true);
+        aliveTree.SetActive(false);
+        GetComponent<Collider2D>().enabled = false;
     }
 }
